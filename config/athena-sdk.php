b8c61a4024e28fdccf07535ed7064b5faf31e8c1
Chris Reed <chris@reedtech.us>
2023-12-04 02:06:12 -0600

    refactor: streamline athena-sdk config structure
    Removed unnecessary 'api' nesting from athena-sdk config, adding 'practice_id' configuration. This change simplifies the configuration access pattern and reduces the depth of the config array, potentially improving readability and ease of use for developers interacting with Athena SDK settings.

--- config/athena-sdk.php
+++ config/athena-sdk.php
@@ -2,9 +2,9 @@
 
 // config for ChrisReedIO/Athena
 return [
-    'api' => [
-        'base_url' => env('ATHENA_BASE_URL', 'https://api.preview.platform.athenahealth.com'),
-        'client_id' => env('ATHENA_CLIENT_ID', ''),
-        'client_secret' => env('ATHENA_CLIENT_SECRET', ''),
-    ],
+    'base_url' => env('ATHENA_BASE_URL', 'https://api.preview.platform.athenahealth.com'),
+    'client_id' => env('ATHENA_CLIENT_ID', ''),
+    'client_secret' => env('ATHENA_CLIENT_SECRET', ''),
+
+    'practice_id' => env('ATHENA_PRACTICE_ID', ''),
 ];
