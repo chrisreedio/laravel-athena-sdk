731c49a0fdfe802467d4aac2598fe1bd6a34ab3d
Chris Reed <chris@reedtech.us>
2023-12-04 04:44:51 -0600

    feat: integrate OAuth2 in Athena connector
    - Added OAuth2 support to Athena SDK
    - Utilize client credentials grant for authentication
    - Configure OAuth2 details using SDK config settings
    - Set default scopes for Athena service access
    
    OAuth2 support enables secure API requests with improved authentication handling, adhering to industry standards. This integration simplifies the process of connecting to the Athena API by abstracting the authentication flow.

--- src/Athena.php
+++ src/Athena.php
@@ -3,10 +3,16 @@
 namespace ChrisReedIO\AthenaSDK;
 
 // use ChrisReedIO\AthenaSDK\Resource\Appointments;
+use Illuminate\Http\Request;
+use OAuth;
+use Saloon\Helpers\OAuth2\OAuthConfig;
 use Saloon\Http\Connector;
+use Saloon\Traits\OAuth2\ClientCredentialsGrant;
 
 class Athena extends Connector
 {
+    use ClientCredentialsGrant;
+
     protected ?string $baseUrl = null;
 
     public function __construct(protected ?string $practiceId = null)
@@ -25,6 +31,19 @@ class Athena extends Connector
     public function resolveBaseUrl(): string
     {
         return $this->baseUrl . '/v1/' . $this->practiceId;
+    }
+
+    protected function defaultOauthConfig(): OAuthConfig
+    {
+        return OAuthConfig::make()
+            ->setClientId(config('athena-sdk.client_id'))
+            ->setClientSecret(config('athena-sdk.client_secret'))
+            // ->setTokenEndpoint('/oauth/token')
+            ->setTokenEndpoint( $this->baseUrl . '/oauth2/v1/token')
+            ->setDefaultScopes(['athena/service/Athenanet.MDP.*']);
+            // ->setRequestModifier(function (Request $request) {
+            //     // $request->headers->set('Accept', 'application/json');
+            // });
     }
 
     // public function appointments(): Appointments
---
+++ src/AthenaAuthenticator.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace ChrisReedIO\AthenaSDK;
+
+// use ChrisReedIO\AthenaSDK\Resource\Appointments;
+use Illuminate\Http\Request;
+use OAuth;
+use Saloon\Helpers\OAuth2\OAuthConfig;
+use Saloon\Http\Connector;
+use Saloon\Traits\OAuth2\ClientCredentialsGrant;
+
+class AthenaAuthenticator extends Connector
+{
+    use ClientCredentialsGrant;
+
+    protected ?string $baseUrl = null;
+
+    public function __construct()
+    {
+        $this->baseUrl = config('athena-sdk.base_url');
+        if (empty($this->baseUrl)) {
+            throw new \Exception('Athena SDK base_url not set in config/athena-sdk.php');
+        }
+    }
+
+    public function resolveBaseUrl(): string
+    {
+        return $this->baseUrl;
+    }
+
+    protected function defaultOauthConfig(): OAuthConfig
+    {
+        return OAuthConfig::make()
+            ->setClientId(config('athena-sdk.client_id'))
+            ->setClientSecret(config('athena-sdk.client_secret'))
+            ->setTokenEndpoint( '/oauth2/v1/token')
+            ->setDefaultScopes(['athena/service/Athenanet.MDP.*'])
+            ->setRequestModifier(function (Request $request) {
+                // $request->headers->set('Accept', 'application/json');
+            });
+    }
+
+}
