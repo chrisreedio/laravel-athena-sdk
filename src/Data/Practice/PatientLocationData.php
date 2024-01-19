6d1aaeb19b4340e0a727088b43509f14c79743df
Chris Reed <chris@reedtech.us>
2024-01-19 14:09:48 -0600

    feat: add patient location list handling
    Implement patient location listing feature
    - Introduce `PatientLocationData` DTO for structured data handling
    - Implement `createDtoFromResponse` to transform API response into Collection of DTOs
    - Update `Departments` resource to return a Collection of `PatientLocationData`
    - This enhances the maintainability and readability of the code by using strong typing and clear data structures

---
+++ src/Data/Practice/PatientLocationData.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace ChrisReedIO\AthenaSDK\Data\Practice;
+
+use ChrisReedIO\AthenaSDK\Data\AthenaData;
+
+readonly class PatientLocationData extends AthenaData
+{
+    public function __construct(
+        public ?int $athenaId = null,
+        public ?string $name = null,
+        public ?bool $isDefault = null,
+    )
+    { }
+
+    public static function fromArray(array $data): static
+    {
+        return new static(
+            athenaId: $data['patientlocationid'] ?? null,
+            name: $data['name'] ?? null,
+            isDefault: $data['defaultoncheckin'] ?? null,
+        );
+    }
+}
