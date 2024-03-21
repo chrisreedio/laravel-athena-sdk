3c89b8753f72805b13200c702540e106d4be496e
chrisreedio <chrisreedio@users.noreply.github.com>
2024-03-21 04:49:28 +0000

    Fix styling

--- src/Data/Patient/ChartAlertData.php
+++ src/Data/Patient/ChartAlertData.php
@@ -19,7 +19,7 @@ readonly class ChartAlertData extends AthenaData
     public static function fromArray(array $data): static
     {
         $modifiedAt = $data['lastmodified'] ?? null;
-        if (!empty($modifiedAt)) {
+        if (! empty($modifiedAt)) {
             // The time this note was updated (mm/dd/yyyy hh24:mi:ss; Eastern time), if the note has been updated.
             // Note the docs are wrong, it's a two digit year
             $modifiedAt = Carbon::createFromFormat('m/d/y H:i:s', $data['lastmodified'], 'America/New_York')
