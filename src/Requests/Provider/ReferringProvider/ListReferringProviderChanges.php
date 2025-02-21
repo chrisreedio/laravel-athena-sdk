5e67453b93187662a7b8931394c739faed19446c
Chris Reed <chris@reedtech.us>
2025-02-21 12:42:25 -0600

    🔄 refactor: adjusted variable naming to camelCase in `ListReferringProviderChanges.php`
    Updated variable names from snake_case to camelCase to match coding standards. This change improves code readability and consistency.

--- src/Requests/Provider/ReferringProvider/ListReferringProviderChanges.php
+++ src/Requests/Provider/ReferringProvider/ListReferringProviderChanges.php
@@ -2,12 +2,11 @@
 
 namespace ChrisReedIO\AthenaSDK\Requests\Provider\ReferringProvider;
 
-use ChrisReedIO\AthenaSDK\Data\Provider\ProviderData;
 use ChrisReedIO\AthenaSDK\Data\Provider\ReferringProviderData;
 use ChrisReedIO\AthenaSDK\PaginatedRequest;
 use Saloon\Enums\Method;
-
 use Saloon\Http\Response;
+
 use function collect;
 use function config;
 
@@ -33,10 +32,11 @@ class ListReferringProviderChanges extends PaginatedRequest
      * @param  null|string  $showprocessedstartdatetime  Show already processed changes.  This will show changes that you previously retrieved at some point after this datetime mm/dd/yyyy hh24:mi:ss (Eastern). Can be used to refetch data if there was an error, such as a timeout, and records are marked as already retrieved. This is intended to be used with showprocessedenddatetime and for a short period of time only. Also note that all messages will eventually be deleted.
      */
     public function __construct(
-        protected ?bool $leaveunprocessed = null,
-        protected ?string $showprocessedenddatetime = null,
-        protected ?string $showprocessedstartdatetime = null,
-    ) {}
+        protected ?bool $leaveUnprocessed = null,
+        protected ?string $showProcessedEndDatetime = null,
+        protected ?string $showProcessedStartDatetime = null,
+    ) {
+    }
 
     public function defaultQuery(): array
     {
@@ -44,8 +44,8 @@ class ListReferringProviderChanges extends PaginatedRequest
 
         return array_filter([
             'leaveunprocessed' => $leave,
-            'showprocessedenddatetime' => $this->showprocessedenddatetime,
-            'showprocessedstartdatetime' => $this->showprocessedstartdatetime,
+            'showprocessedenddatetime' => $this->showProcessedEndDatetime,
+            'showprocessedstartdatetime' => $this->showProcessedStartDatetime,
         ]);
     }
 
