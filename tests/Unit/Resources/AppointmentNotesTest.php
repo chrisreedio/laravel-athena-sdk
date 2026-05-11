7874dc82261eb86d535707318a8a2bb74027a41f
Chris Reed <chris@reedtech.us>
2026-05-11 12:08:43 -0500

    Refactor AppointmentNotesTest to correct namespace usage
    - Updated the namespace import for AppointmentNotesResource in AppointmentNotesTest.php to ensure proper referencing of the AppointmentNotes resource.

--- tests/Unit/Resources/AppointmentNotesTest.php
+++ tests/Unit/Resources/AppointmentNotesTest.php
@@ -2,8 +2,8 @@
 
 use ChrisReedIO\AthenaSDK\AthenaConnector;
 use ChrisReedIO\AthenaSDK\Requests\Appointments\AppointmentNotes\CreateAppointmentNote;
-use ChrisReedIO\AthenaSDK\Resources\Appointments\AppointmentNotes as AppointmentNotesResource;
 use ChrisReedIO\AthenaSDK\Resources\Appointments;
+use ChrisReedIO\AthenaSDK\Resources\Appointments\AppointmentNotes as AppointmentNotesResource;
 use Saloon\Http\Faking\MockClient;
 use Saloon\Http\Faking\MockResponse;
 
