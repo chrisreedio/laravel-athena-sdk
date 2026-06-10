79ead2fc8d27bcc52434b3a8e1815986eb3d199a
Christopher Graham <chgraham@springfieldclinic.com>
2026-06-10 22:57:48 +0300

    fix(patient): change athenaLocalId type from int to string
    - Updated the `athenaLocalId` property in `PatientData` to accept a string instead of an integer.
    - Adjusted the `fromArray` method to reflect this change, ensuring proper handling of local patient IDs.

--- src/Data/Patient/PatientData.php
+++ src/Data/Patient/PatientData.php
@@ -13,7 +13,7 @@ readonly class PatientData extends AthenaData
      */
     public function __construct(
         public ?int $athenaId = null,
-        public ?int $athenaLocalId = null,
+        public ?string $athenaLocalId = null,
         public ?string $firstName = null,
         public ?string $lastName = null,
         public ?string $sex = null,
@@ -63,7 +63,7 @@ readonly class PatientData extends AthenaData
     {
         return new static(
             athenaId: $data['patientid'] ?? null,
-            athenaLocalId: isset($data['localpatientid']) ? (int) $data['localpatientid'] : null,
+            athenaLocalId: isset($data['localpatientid']) ? $data['localpatientid'] : null,
             firstName: $data['firstname'] ?? null,
             lastName: $data['lastname'] ?? null,
             sex: $data['sex'] ?? null,
